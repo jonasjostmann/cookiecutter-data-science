3ebe9c9fc7fa3069e465251355af3fd9fa51795d
Jonas Jostmann <j_jost04@uni-muenster.de>
2020-10-28 19:17:40 +0100

    Replaced requirments.txt by pipenv structure

--- tests/test_creation.py
+++ tests/test_creation.py
@@ -69,15 +69,6 @@ class TestCookieSetup(object):
         else:
             assert p == 'MIT'
 
-    def test_requirements(self):
-        reqs_path = self.path / 'requirements.txt'
-        assert reqs_path.exists()
-        assert no_curlies(reqs_path)
-        if pytest.param.get('python_interpreter'):
-            with open(reqs_path) as fin:
-                lines = list(map(lambda x: x.strip(), fin.readlines()))
-            assert 'pathlib2' in lines
-
     def test_makefile(self):
         makefile_path = self.path / 'Makefile'
         assert makefile_path.exists()
